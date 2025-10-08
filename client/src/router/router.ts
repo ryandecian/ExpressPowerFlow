acd6fd4317f23ced10738bb1d6b33b308e48e138
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-09 00:53:04 +0200

    Création du router de manière partiel

---
+++ client/src/router/router.ts
@@ -0,0 +1,13 @@
+/* Import des composants React */
+import { ReactNode } from "react"
+
+/* Liste des pages en import */
+/* Page Frontend vérrouillée */
+
+/* Page Frontend public */
+
+/**
+ * Utilisation : 
+ * {router[0].path}
+ * {routerExt.instagram}
+ */
