37f1bf45aa6792a4e5f5522145aed74816cd71c0
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-24 12:49:17 +0200

    Mise a jour de clientOptions_MQTT

--- server/src/mqtt/mqttClientExpress.config.mqtt.ts
+++ server/src/mqtt/mqttClientExpress.config.mqtt.ts
@@ -1,12 +1,15 @@
+/* Import des Config : */
+import { ENV_SAFE } from "../config/ENV.config.js";
+
 /* Import des Types : */
 import type { IClientOptions } from "mqtt";
 
-const MQTT_USER = process.env.MQTT_USER ?? "express_power_flow";
-const MQTT_PASSWORD = process.env.MQTT_PASSWORD ?? "super_secure_password";
+const MQTT_USER_EXPRESSPOWERFLOW = ENV_SAFE("MQTT_USER_EXPRESSPOWERFLOW");
+const MQTT_PASSWORD_EXPRESSPOWERFLOW = ENV_SAFE("MQTT_PASSWORD_EXPRESSPOWERFLOW");
 
 const clientOptions_MQTT: IClientOptions = {
-    username: MQTT_USER,           /* Identifiant du client MQTT */
-    password: MQTT_PASSWORD,       /* Mot de passe d’authentification */
+    username: MQTT_USER_EXPRESSPOWERFLOW,           /* Identifiant du client MQTT */
+    password: MQTT_PASSWORD_EXPRESSPOWERFLOW,       /* Mot de passe d’authentification */
     clean: true,                   /* Session non persistante */
     reconnectPeriod: 1000,         /* Tente de se reconnecter toutes les 1 seconde */
     connectTimeout: 10_000,        /* Délai maximum pour établir la connexion (10s) */
