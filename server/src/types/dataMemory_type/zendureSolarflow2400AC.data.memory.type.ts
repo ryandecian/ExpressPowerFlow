6627a10804e8604bd82e44c1866f36779c8fda56
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-26 22:43:46 +0100

    Création de ZendureSolarflow2400AC_data_memory_Type

--- server/src/types/dataMemory_type/zendureSolarflow2400AC.data.memory.type.ts
+++ server/src/types/dataMemory_type/zendureSolarflow2400AC.data.memory.type.ts
@@ -2,7 +2,17 @@ type ZendureSolarflow2400AC_data_memory_Type = {
     timestamp: number, /* ⚠️ Horodatage UNIX du message (en secondes) */
     sn: string, /* ⚠️ Numéro de série de l’appareil principal (onduleur SolarFlow 2400 AC) */
     product: string, /* ⚠️ Nom du modèle de l’appareil Zendure */
-    properties: {};
+    properties: {
+        electricLevel: number, /* ⚠️ Niveau de charge batterie (en %) */
+        hyperTmp: number, /* ⚠️ Température interne brute (3071 = 30,71°C) */
+        gridState: number, /* ⚠️ État du réseau détecté (1 = présent) */
+        BatVolt: number, /* ⚠️ Tension batterie (mV → 4971 ≈ 49,71V) */
+        acMode: number, /* ⚠️ Mode AC: 0 = veille/idle, 1 = charge, 2 = décharge */
+        inputLimit: number, /* ⚠️ Limite de puissance d’entrée configurée (W ou %) */
+        outputLimit: number, /* ⚠️ Limite de puissance de sortie configurée vers maison (W) */
+        socSet: number, /* ⚠️ SoC cible paramétré (1000 ≈ 100%) */
+        minSoc: number, /* ⚠️ Seuil minimum avant arrêt de décharge (50 ≈ 5%) */
+    };
 };
 
 export { ZendureSolarflow2400AC_data_memory_Type };
