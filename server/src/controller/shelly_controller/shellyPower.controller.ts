d8ae05bfc2e07e33b1e821b8ebdb8533487c8b5e
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-27 22:43:40 +0100

    Création du controller shellyPower_Controller

--- server/src/controller/shelly_controller/shellyPower.controller.ts
+++ server/src/controller/shelly_controller/shellyPower.controller.ts
@@ -1,14 +1,35 @@
 /* Import des Datas */
+import { get } from "http";
 import { setShelly3EMSnapshot } from "../../database/data_memory/memory.data.js";
+import { getShelly3EMSnapshot } from "../../database/data_memory/memory.data.js";
 
 /* Import des Types : */
+import { GetShelly3EM_emeter_data_Type } from "../../types/dataFetch_type/getShelly3EM.emeter.data.type.js";
 
 /* Import des Utils */
 import { fetch_Utils } from "../../utils/fetch.utils.js";
 
-function shellyPower_Controller(): Promise<void> {
+const SHELLY_URL = "http://192.168.1.23/emeter/0";
+
+async function shellyPower_Controller(): Promise<void> {
     try {
         /* Logique métier 1 : Récupération des données du compteur */
+            const dataShellyResult = await fetch_Utils<GetShelly3EM_emeter_data_Type>("GET", SHELLY_URL);
+
+            /* Vérification si le fetch a échoué */
+            if (typeof dataShellyResult.error === "string") {
+                console.error("shellyPower_Controller - Erreur de fetch :", dataShellyResult.error);
+            }
+
+            const dataShelly = dataShellyResult.data as GetShelly3EM_emeter_data_Type;
+
+        /* Logique métier 2 : Enregistrement des données dans la mémoire */
+            setShelly3EMSnapshot(dataShelly);
+
+        /* Logique métier 3 : Récupération des données depuis la mémoire pour vérification */
+            const data = getShelly3EMSnapshot();
+
+            console.log(`Compteur Shelly 3EM : ${data?.data.power} W`);
     }
     catch (error) {
         console.error("Erreur dans shellyPower_Controller :", error);
