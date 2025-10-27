24d77158e99ee71ba87efb21e2e120533b1bdbdf
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-10-27 22:50:38 +0100

    Mise a jour de shellyPower_Controller

--- server/src/controller/shelly_controller/shellyPower.controller.ts
+++ server/src/controller/shelly_controller/shellyPower.controller.ts
@@ -1,11 +1,10 @@
 /* Import des Datas */
-import { get } from "http";
 import { setShelly3EMSnapshot } from "../../database/data_memory/memory.data.js";
 import { getShelly3EMSnapshot } from "../../database/data_memory/memory.data.js";
 
 /* Import des Types : */
-import { GetShelly3EM_emeter_data_Type } from "../../types/dataFetch_type/getShelly3EM.emeter.data.type.js";
-
+import type { GetShelly3EM_emeter_data_Type } from "../../types/dataFetch_type/getShelly3EM.emeter.data.type.js";
+import type { Shelly3EM_data_memory_Type } from "../../types/dataMemory_type/shelly3EM.data.memory.type.js";
 /* Import des Utils */
 import { fetch_Utils } from "../../utils/fetch.utils.js";
 
@@ -23,10 +22,18 @@ async function shellyPower_Controller(): Promise<void> {
 
             const dataShelly = dataShellyResult.data as GetShelly3EM_emeter_data_Type;
 
-        /* Logique métier 2 : Enregistrement des données dans la mémoire */
-            setShelly3EMSnapshot(dataShelly);
+        /* Logique métier 2 : Préparation des données pour l'enregistrement */
+            const dataSelected: Shelly3EM_data_memory_Type = {
+                power: dataShelly.power,
+                pf: dataShelly.pf,
+                current: dataShelly.current,
+                voltage: dataShelly.voltage,
+            }
 
-        /* Logique métier 3 : Récupération des données depuis la mémoire pour vérification */
+        /* Logique métier 3 : Enregistrement des données dans la mémoire */
+            setShelly3EMSnapshot(dataSelected);
+
+        /* Logique métier 4 : Récupération des données depuis la mémoire pour vérification */
             const data = getShelly3EMSnapshot();
 
             console.log(`Compteur Shelly 3EM : ${data?.data.power} W`);
---
+++ server/src/controller/shelly_controller/shellyPriseZendure.controller.ts
@@ -0,0 +1,3 @@
+
+
+async function shellypriseZendure_Controller(): Promise<void> {}
