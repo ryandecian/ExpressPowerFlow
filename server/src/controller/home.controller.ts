abcc7e1465106e5f5e0a2e8899838a237c133434
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-02 22:44:28 +0100

    Fin de la création du controller home_Controller, on peut commencer les test

--- server/src/controller/home.controller.ts
+++ server/src/controller/home.controller.ts
@@ -1,13 +1,19 @@
 
 /* Import des Datas */
+import { get } from "http";
 import { getShelly3EMSnapshot } from "../database/data_memory/memory.data.js";
 import { getShellyPlugSGen3_BatterieZSF2400AC_1_Snapshot } from "../database/data_memory/memory.data.js";
+import { getZendureSolarflow2400AC_1_Snapshot } from "../database/data_memory/memory.data.js";
+
+/* Import des Types : */
+import type { PostZendureSolarflow2400AC_data_Type } from "../types/dataFetch_type/postZendureSorlarflow2400AC.data.type.js";
 
 /* Import des Utils */
 import { adjustZendureChargePower } from "../utils/ajustement/adjustZendureChargePower.utils.js";
 import { adjustZendureDischargePower } from "../utils/ajustement/adjustZendureDischargePower.utils.js";
 import { fetch_Utils } from "../utils/fetch.utils.js";
 
+const ZENDURE_URL_POST = "http://192.168.1.26/properties/write";
 
 async function home_Controller(): Promise<void> {
     try {
@@ -39,20 +45,38 @@ async function home_Controller(): Promise<void> {
         /* Logique métier 4 : Préparation de la commande à envoyer aux batteries */
             let commande: number = 0;
             const homePowerAbs = Math.abs(homePower); /* Conversion de la valeur en positif */
+            let body = {};
 
             /* Si la consommation de la maison est négative, on charge la batterie */
             if (homePower < 0) {
                 commande = adjustZendureChargePower(homePowerAbs);
+                body = {
+                    sn: getZendureSolarflow2400AC_1_Snapshot()?.data.sn, /* Numéro de série de l'appareil cible */
+                    properties: {
+                        acMode: 1, /* Commande charge */
+                        inputLimit: commande, /* Commande : puissance de charge demandée */
+                    }
+                };
             }
 
             /* Si la consommation de la maison est positive, on décharge la batterie */
             if (homePower > 0) {
                 commande = adjustZendureDischargePower(homePowerAbs);
+                body = {
+                    sn: getZendureSolarflow2400AC_1_Snapshot()?.data.sn, /* Numéro de série de l'appareil cible */
+                    properties: {
+                        acMode: 2, /* Commande décharge */
+                        outputLimit: commande, /* Commande : puissance de décharge demandée */
+                    }
+                };
             }
 
         /* Logique métier 5 : Envoi de la commande aux batteries */
-            if (homePower < 0) {
-                const body = {}
+            const postZendureResult = await fetch_Utils<PostZendureSolarflow2400AC_data_Type>("POST", ZENDURE_URL_POST, body);
+
+            if (typeof postZendureResult.error === "string") {
+                console.error("Une erreur est survenue lors de l'envoi de la commande à la Batterie Zendure Solarflow 2400 AC :", postZendureResult.error);
+                return;
             }
     }
     catch (error) {
