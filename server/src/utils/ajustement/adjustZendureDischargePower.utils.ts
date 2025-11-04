e094b663fc8719d601d5b79b79cc1ce80abbd3af
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-11-04 08:14:41 +0100

    Mise a jour des utils permettant de calculer la bonne valeur a envoyer en commande aux batteries

--- server/src/utils/ajustement/adjustZendureDischargePower.utils.ts
+++ server/src/utils/ajustement/adjustZendureDischargePower.utils.ts
@@ -1,5 +1,12 @@
+/* Cette fonction reçois un nombre positif ou négatif */
 function adjustZendureDischargePower(targetDischarge: number): number {
-    let commande = (targetDischarge - 22.7) / 1.03;
+    // let commande = (targetDischarge - 22.7) / 1.03;
+    let commande = (targetDischarge * 0.975) + 21;
+
+    /* Si la valeur est négative, on la change en positif */
+    if (commande < 0) {
+        commande = Math.abs(commande);
+    }
 
     /* Limite la commande a la puissance maximale de décharge de la batterie */
     if (commande > 2400) {
@@ -9,6 +16,7 @@ function adjustZendureDischargePower(targetDischarge: number): number {
     /* Arrondi la au nombre entier le plus proche */
     commande = Math.round(commande);
 
+    /* Retourne la commande ajustée tjs positive */
     return commande;
 }
 
