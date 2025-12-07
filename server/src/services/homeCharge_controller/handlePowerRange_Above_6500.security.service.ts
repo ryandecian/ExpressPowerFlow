2ad3d4bc9b03bc35a3d6cc2241aad011070dea77
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-12-07 23:56:47 +0100

    Push partiel de la logique de handlePowerRange_Above_6500_security_Service

--- server/src/services/homeCharge_controller/handlePowerRange_Above_6500.security.service.ts
+++ server/src/services/homeCharge_controller/handlePowerRange_Above_6500.security.service.ts
@@ -16,11 +16,36 @@ function handlePowerRange_Above_6500_security_Service(
     
     /* Calcul du seul de déclanchement */
         const maxPowerHome: number = 6500; /* Puissance à atteindre */
-        const thresholdPower: number = homePower - maxPowerHome;
+        const thresholdPower: number = homePower - maxPowerHome; /* Nombre positif, il faudra le mettre négatif pour décharger les batteries */
 
     /* Couche 1 : Si le seuil de puissance est dépassé de plus de 100w */
         if (thresholdPower >= 100) {
             /* Couche 2 : Les deux batteries sont disponibles */
+                if (selectBattery.zendureSolarflow2400AC_N1.status === true && selectBattery.zendureSolarflow2400AC_N2.status === true) {
+                    /* Couche 3 : Cas 1 : Les 2 batteries ont des niveaux de charge identiques */
+                        if (selectBattery.zendureSolarflow2400AC_N1.electricLevel === selectBattery.zendureSolarflow2400AC_N2.electricLevel) {
+                            body.ZSF2400AC_N1 = requestZSF2400AC_Utils(selectBattery.zendureSolarflow2400AC_N1.sn, -thresholdPower * 0.5);
+                            body.ZSF2400AC_N2 = requestZSF2400AC_Utils(selectBattery.zendureSolarflow2400AC_N2.sn, -thresholdPower * 0.5 + 5);
+                        }
+                    /* Couche 3 : Cas 2 : La batterie N1 a un niveau de charge plus haut que la batterie N2 */
+                        else if (selectBattery.zendureSolarflow2400AC_N1.electricLevel > selectBattery.zendureSolarflow2400AC_N2.electricLevel) {
+                            const deltaElectricLevel: number = selectBattery.zendureSolarflow2400AC_N1.electricLevel - selectBattery.zendureSolarflow2400AC_N2.electricLevel;
+
+                            /* Si la différence de % est de 5 ou plus la batterie N1 réduit sa puissance en priorité */
+                                if (deltaElectricLevel >= 5) {
+                                    /* Si la puissance max de charge (thresholdPower) disponible est suppérieur a 2400w */
+                                        if (thresholdPower > 2400) {
+                                            body.ZSF2400AC_N1 = requestZSF2400AC_Utils(selectBattery.zendureSolarflow2400AC_N1.sn, -2400);
+                                            body.ZSF2400AC_N2 = requestZSF2400AC_Utils(selectBattery.zendureSolarflow2400AC_N2.sn, -thresholdPower + 2400 + 5);
+                                        }
+                                    /* Si la puissance max de charge est inférieur ou égale à 2400w */
+                                        else if (thresholdPower <= 2400 && thresholdPower > 0) {
+                                            body.ZSF2400AC_N1 = requestZSF2400AC_Utils(selectBattery.zendureSolarflow2400AC_N1.sn, -thresholdPower);
+                                            body.ZSF2400AC_N2 = requestZSF2400AC_Utils(selectBattery.zendureSolarflow2400AC_N2.sn, 0); /* Commande pour mise en veille */
+                                        }
+                                }
+                        }
+                }
             /* Couche 2 : Seul la batterie N1 est disponible */
                 else if (selectBattery.zendureSolarflow2400AC_N1.status === true) {
                     /* Attribution de toute la puissance à la batterie N1 */
