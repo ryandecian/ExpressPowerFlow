8b9fb0cb3338c005e3f99b3c6af5016a2cd3a86b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-04 21:05:15 +0100

    Mise a jour de zendureSolarflow2400ACN2_Controller

--- server/src/controller/manager_controller/homeManager.controller.ts
+++ server/src/controller/manager_controller/homeManager.controller.ts
@@ -1,7 +1,16 @@
 
 
 async function homeManager_Controller(): Promise<void> {
-    try {}
+    try {
+        /* Logique métier 1 : Initialisation des variables */
+            let shellyPro3EM_Controle: boolean = false;
+            let shellyPriseZSF2400ACN1_Controle: boolean = false;
+            let shellyPriseZSF2400ACN2_Controle: boolean = false;
+            let zendureSolarflow2400ACN1_Controle: boolean = false;
+            let zendureSolarflow2400ACN2_Controle: boolean = false;
+
+        /* Logique métier 2 : Lancement des controller en même temps */
+    }
     catch (error) {
         console.error(`[homeManager_Controller] - Une erreur inconnue est survenue : `, error);
     }
