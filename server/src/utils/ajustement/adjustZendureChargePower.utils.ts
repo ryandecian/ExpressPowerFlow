e200e100cd1c3ef56ccb154ad3e5fb3b81ece534
Ryan DECIAN <ryan.decian.pro@gmail.com>
2026-01-05 22:39:36 +0100

    Mise a jour de la formule de adjustZendureChargePower

--- server/src/utils/ajustement/adjustZendureChargePower.utils.ts
+++ server/src/utils/ajustement/adjustZendureChargePower.utils.ts
@@ -1,6 +1,6 @@
 /* Cette fonction reçois un nombre positif donc charge de la batterie */
 function adjustZendureChargePower(targetCharge: number): number {
-    let commande = (targetCharge * 0.966) + 22;
+    let commande = (targetCharge * 0.973154) + 21.0537;
 
     /* Si la valeur est négative, on la change en positif */
     if (commande < 0) {
